356faf528fead4ff7d3e58b419a3998a6b5fc608
JoseR200 <joserodrigolopez@icloud.com>
2025-08-15 11:04:27 +0200

    Imports

--- src/core/products/products.service.ts
+++ src/core/products/products.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {Product} from "./products.entity";
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+
+    constructor(
+        @InjectRepository(Product)
+        private readonly productRepository: Repository<Product>,
+    ) {}
+}
